6e65e0692863a206362360bf48f73d6c92dd8fb3
datnd35 <datitk16@gmail.com>
2026-06-14 09:45:41 +0700

    feat: add diver map clustering demo
    Add Vue 3 + OpenLayers clustering demo with Diver markers.
    Supports single marker (blue, shows 1) and cluster marker (red, shows count).
    Click interactions show diver detail or grouped list popup.

---
+++ he-clustering-demo/src/data/diverData.js
@@ -0,0 +1,32 @@
+export const diverData = [
+  {
+    id: 'DVR-001',
+    name: 'Diver 001',
+    longitude: 106.700981,
+    latitude: 10.776889,
+  },
+  {
+    id: 'DVR-002',
+    name: 'Diver 002',
+    longitude: 106.700981,
+    latitude: 10.776889,
+  },
+  {
+    id: 'DVR-003',
+    name: 'Diver 003',
+    longitude: 106.700981,
+    latitude: 10.776889,
+  },
+  {
+    id: 'DVR-004',
+    name: 'Diver 004',
+    longitude: 106.705,
+    latitude: 10.78,
+  },
+  {
+    id: 'DVR-005',
+    name: 'Diver 005',
+    longitude: 106.71,
+    latitude: 10.782,
+  },
+]
